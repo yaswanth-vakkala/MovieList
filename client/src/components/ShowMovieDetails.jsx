ce3a626908bd78601d1d65610214c59a5e6dccd2
yaswanth-vakkala <yaswanthv87@gmail.com>
2023-02-27 15:01:46 +0530

    fix deleteclick function-3

--- client/src/components/ShowMovieDetails.jsx
+++ client/src/components/ShowMovieDetails.jsx
@@ -11,7 +11,7 @@ const ShowMovieDetails = (props) => {
 
   useEffect(() => {
     axios
-      .get(import.meta.env.VITE_REACT_API + `/${id}`)
+      .get(import.meta.env.VITE_REACT_API + '/' + `${id}`)
       .then((res) => {
         setMovie(res.data);
       })
